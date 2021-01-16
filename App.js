c9e5831f215b1f788c753e01d0f3cc461f0e48df
Eamon Niknafs <en@bu.edu>
2021-01-16 00:02:15 -0800

    Added camera support.  - Barely functional, but it's a start.

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Cam from "./Cam";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Cam styles={styles}/>
       <StatusBar style="auto" />
     </View>
   );
